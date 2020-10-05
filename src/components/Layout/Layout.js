ffbac305126e19101b97d19c2bac935687a54684
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-10-05 14:05:44 +0530

    uploading files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Aux from "../../hoc/Auxillary";
+import classes from "./Layout.module.css";
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SiderDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
